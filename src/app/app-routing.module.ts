15944f059e9a50277ac2a00ea81d731887688cc8
serhat uygun <serhatuygun54@gmail.com>
2025-01-05 20:06:12 +0300

    yeni sayfalar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { FeaturesComponent } from './features/features.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Ana sayfa
+  { path: 'features', component: FeaturesComponent }, // Özellikler sayfası
+  { path: 'contact', component: ContactComponent }, // İletişim sayfası
+  { path: 'portfolio', component: PortfolioComponent }, // Portföy sayfası
+  { path: 'services', component: ServicesComponent } // Hizmetler sayfası
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
